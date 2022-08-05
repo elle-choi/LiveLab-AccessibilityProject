dc1f2cc8c15708485802bb9a5c613ef4ecc81b76
elle924 <jeong.eun.choi@vanderbilt.edu>
2022-08-05 14:12:42 -0500

    abc

--- Assets/Scripts/StartEndAudio.cs
+++ Assets/Scripts/StartEndAudio.cs
@@ -11,26 +11,28 @@ public class StartEndAudio : MonoBehaviour
 
     private void Start()
     {
-        startSound = GetComponent<AudioSource>();
-        endSound = GetComponent<AudioSource>();
+        //startSound = GetComponent<AudioSource>();
+        //endSound = GetComponent<AudioSource>();
     }
 
     private void Update()
     {
+        /*
         // FIXME: temporary code for now
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            startSound.Play();
+            //Debug.Log("alpha 1 pressed");
+            startSound.Play(0);
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            endSound.Play();
+            endSound.Play(0);
         }
-
+        */
 
         // FIXME: when bottom platform hit, start audio play; last step hit, end audio play
-        /*
+        
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -50,7 +52,7 @@ public class StartEndAudio : MonoBehaviour
                 }
             }
         }
-        */
+        
 
     }
 }
