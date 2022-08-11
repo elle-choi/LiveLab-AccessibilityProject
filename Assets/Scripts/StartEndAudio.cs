f1d67fd9db4faae5046126719f413e7b58ccd2b6
elle924 <jeong.eun.choi@vanderbilt.edu>
2022-08-11 12:46:41 -0500

    8/11

--- Assets/Scripts/StartEndAudio.cs
+++ Assets/Scripts/StartEndAudio.cs
@@ -33,6 +33,7 @@ public class StartEndAudio : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
         RaycastHit hitInfo;
 
+
         if (Physics.Raycast(ray, out hitInfo))
         {
             if (hitInfo.collider.gameObject.tag == "FiveStepTrigger")
@@ -70,6 +71,85 @@ public class StartEndAudio : MonoBehaviour
                 EndStairs.Play(0);
             }
         }
+
+        // wasn't sure what the tag was
+        // for 5-Step stairs but do I need this all for 7 and 9?
+        /* 
+        if (Collider.gameobject.tag == "Up")
+        {
+            if (hitInfo.collider.gameObject.tag == "FiveStepTrigger")
+            {
+                FiveStepStart.Play(0);
+            }
+
+            if (hitInfo.collider.gameObject.tag == "One")
+            {
+                One.Play(0);
+            }
+
+            if (hitInfo.collider.gameObject.tag == "Two")
+            {
+                Two.Play(0);
+            }
+
+            if (hitInfo.collider.gameObject.tag == "Three")
+            {
+                Three.Play(0);
+            }
+
+            if (hitInfo.collider.gameObject.tag == "Four")
+            {
+                Four.Play(0);
+            }
+
+            if (hitInfo.collider.gameObject.tag == "Five")
+            {
+                Five.Play(0);
+            }
+
+            if (hitInfo.collider.gameObject.tag == "EndStairs")
+            {
+                EndStairs.Play(0);
+            }
+        }
+        if (Collider.gameobject.tag == "Down")
+        {
+            if (hitInfo.collider.gameObject.tag == "FiveStepTrigger")
+            {
+                EndStairs.Play(0);
+            }
+
+            if (hitInfo.collider.gameObject.tag == "One")
+            {
+                Five.Play(0);
+            }
+
+            if (hitInfo.collider.gameObject.tag == "Two")
+            {
+                Four.Play(0);
+            }
+
+            if (hitInfo.collider.gameObject.tag == "Three")
+            {
+                Three.Play(0);
+            }
+
+            if (hitInfo.collider.gameObject.tag == "Four")
+            {
+                Two.Play(0);
+            }
+
+            if (hitInfo.collider.gameObject.tag == "Five")
+            {
+                One.Play(0);
+            }
+
+            if (hitInfo.collider.gameObject.tag == "EndStairs")
+            {
+                FiveStepStart.Play(0);
+            }
+        }
+
     }
 
     /*
@@ -79,23 +159,23 @@ public class StartEndAudio : MonoBehaviour
     }
     */
 
-    /*
-    private void Update()
-    {
-        // FIXME: temporary code for now
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        /*
+        private void Update()
         {
-            //Debug.Log("alpha 1 pressed");
-            startSound.Play(0);
-        }
+            // FIXME: temporary code for now
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                //Debug.Log("alpha 1 pressed");
+                startSound.Play(0);
+            }
 
-        if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            endSound.Play(0);
-        }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                endSound.Play(0);
+            }
 
-        // FIXME: when bottom platform hit, start audio play; last step hit, end audio play 
+            // FIXME: when bottom platform hit, start audio play; last step hit, end audio play 
+        }
+        */
+
     }
-    */
-
-}
